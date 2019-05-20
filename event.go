bcf5640c7633b7c0bd8dc8998b73a73181029f4f
dak425 <highlordazurai425@gmail.com>
2019-05-19 22:28:54 -0400

    Added events to mirror new methods on Book interface

--- event.go
+++ event.go
@@ -1,5 +1,24 @@
 package ledger
 
+type WalletFundsTransferred struct {
+	Source      string `json:"source"`
+	Destination string `json:"destination"`
+	Amount      int32  `json:"amount"`
+	Aggregate   string `json:"aggregate"`
+}
+
+type WalletFundsDeposited struct {
+	Wallet    string `json:"wallet"`
+	Deposit   int32  `json:"deposit"`
+	Aggregate string `json:"aggregate"`
+}
+
+type WalletFundsWithdrawn struct {
+	Wallet    string `json:"wallet"`
+	Withdraw  int32  `json:"withdraw"`
+	Aggregate string `json:"aggregate"`
+}
+
 type CreditTransactionAdded struct {
 	Wallet    string `json:"wallet"`
 	Credit    int32  `json:"credit"`
